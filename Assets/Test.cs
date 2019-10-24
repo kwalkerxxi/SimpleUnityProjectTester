89e6e9232a2d514a73800a6ae7bc147ed2af943f
Mark Hoey <batteryyrettab@hotmail.com>
2019-10-24 14:48:02 +1000

    Simple Object Creation

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
     }
 
     // Update is called once per frame
